a428741f47d3a248e23dbb6ec9ac12bebbc19bed
Alexandre-SDV <alexandre.gouraud@supdevinci-edu.fr>
2025-03-04 21:48:15 +0100

    Importation de la "Navbar" dans "App.jsx"
    Importation de la "Navbar" dans "App.jsx"

--- Portfolio-React/src/App.jsx
+++ Portfolio-React/src/App.jsx
@@ -1,11 +1,12 @@
 ReadableStreamDefaultController
 
 import React from 'react';
+import Navbar from './Components/Navbar';
 
 const App = () => {
   return (
     <div>
-
+      <Navbar/>
     </div>
   );
 }
