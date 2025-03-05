61100f1cebb8125e10bcebc73534153a9df3ca90
Alexandre-SDV <alexandre.gouraud@supdevinci-edu.fr>
2025-03-05 17:45:25 +0100

    Création du Composant "Hero" + Correction code
    Création du Composant "Hero" + Correction code App.jsx et index.css

--- Portfolio-React/src/App.jsx
+++ Portfolio-React/src/App.jsx
@@ -1,12 +1,12 @@
-ReadableStreamDefaultController
-
 import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
+import Hero from './Components/Navbar/Hero/Hero';
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Hero/>
     </div>
   );
 }
