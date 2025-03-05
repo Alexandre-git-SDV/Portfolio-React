15688e542142669dcb16df8c94b9c84c59494077
Alexandre-SDV <alexandre.gouraud@supdevinci-edu.fr>
2025-03-05 18:27:53 +0100

    Correction composant "Hero"
    Correction composant "Hero"

--- Portfolio-React/src/Components/Navbar/Hero/Hero.jsx
+++ Portfolio-React/src/Components/Navbar/Hero/Hero.jsx
@@ -6,10 +6,10 @@ const Hero = () => {
   return (
     <div className = 'hero'>
         <img src={profile_img} alt="" />
-        <h1><span>Je suis Alexandre GOURAUD</span>, un jeune développeur FullStack</h1>
+        <h1><span>Alexandre GOURAUD</span>, un jeune développeur FullStack</h1>
         <p>Je suis actuellement en recherche d'alternance donc si vous recherchez un développeur motivé et autodidacte je suis juste là</p>
         <div className='hero-action'>
-            <div className='hero-connect'>Contacter Moi</div>
+            <div className='hero-connect'>Contactez Moi</div>
             <div className='hero-resume'>Voici mon CV</div>
         </div>
     </div>
