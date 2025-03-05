8709a4409a9edea12ea55696641ab048236da88d
Alexandre-SDV <alexandre.gouraud@supdevinci-edu.fr>
2025-03-05 18:01:07 +0100

    Changement de fichier de profile
    Changement de fichier de profile en utilisant un jpg au lieu d'un svg

--- Portfolio-React/src/Components/Navbar/Hero/Hero.jsx
+++ Portfolio-React/src/Components/Navbar/Hero/Hero.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './Hero.css'
-import profile_img from '../../assets/profile_img.svg'
+import profile_img from '../../../assets/profile_img.jpg'
 
 const Hero = () => {
   return (
     <div className = 'hero'>
         <img src={profile_img} alt="" />
+        <h1>Je suis Alexandre GOURAUD, un jeune développeur FullStack</h1>
+        <p>Je suis actuellement en recherche d'alternance donc si vous recherchez un développeur motivé et autodidacte je suis juste là</p>
+        <div className='hero-action'>
+            <div className='hero-connect'>Contacter Moi</div>
+            <div className='hero-resume'>Voici mon CV</div>
+        </div>
     </div>
   )
 }
