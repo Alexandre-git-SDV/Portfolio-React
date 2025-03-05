1b8667e804c8e123f55c3f6eb444370122c7c04b
Alexandre-SDV <alexandre.gouraud@supdevinci-edu.fr>
2025-03-05 22:39:14 +0100

    Modification de "Navbar" et "Hero"
    Modification de "Navbar" et "Hero"
    
    Modification des transitions et titres

--- Portfolio-React/src/Components/Hero/Hero.jsx
+++ Portfolio-React/src/Components/Hero/Hero.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Hero.css'
-import profile_img from '../../../assets/profile_img.jpg'
+import profile_img from '../../assets/profile_img.jpg'
 
 const Hero = () => {
   return (
     <div className = 'hero'>
         <img src={profile_img} alt="" />
-        <h1><span>Alexandre GOURAUD</span>, un jeune développeur FullStack</h1>
+        <h1><span>Alexandre GOURAUD</span></h1>
+        <h1>Développeur FullStack</h1>
         <p>Je suis actuellement en recherche d'alternance donc si vous recherchez un développeur motivé et autodidacte je suis juste là</p>
         <div className='hero-action'>
             <div className='hero-connect'>Contactez Moi</div>
